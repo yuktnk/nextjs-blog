5c36c96d43bbc95c843ed4c766e14512fbfe310f
yuktnk <yuki0wl@gmail.com>
2020-11-25 16:23:31 +0900

    remark、dete-fnsを用いてビューを整理、トップページから各ページへのリンクを設定

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,31 +1,49 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
+import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
+
+// 1, ページをrenderingするcomponent
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
 
+// 2, getStaticPaths
 export async function getStaticPaths() {
   const paths = getAllPostIds()
   return {
-    paths,
-    fallback: false
+    paths,          // 事前にビルドするパス対象を指定するための配列
+    fallback: false // 事前ビルドしたパス以外にアクセスしたときの動作を決めるもの（falseの場合は404ページを表示）
   }
 }
 
+// 3, getStaticProps
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
     }
   }
 }
+
+
+// The page file must contain :
+// 1, A React component to render this page
+// 2, getStaticPaths which returns an array of possible values for id
+// 3, getStaticProps which fetches necessary data for the post with id
